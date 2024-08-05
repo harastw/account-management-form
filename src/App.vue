2e47cded4de09b4a4b443d3d54a9325fd5f5c28b
Sark <uitrinken1@gmail.com>
2024-08-05 23:25:03 +0700

    fix bug with unread param props

--- src/App.vue
+++ src/App.vue
@@ -39,13 +39,9 @@ export default class App extends Vue {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // color: #2c3e50;
+  color: #2c3e50;
   margin-top: 60px;
   margin-left: 60px;
-}
-
-#headlines {
- 
 }
 
 </style>
