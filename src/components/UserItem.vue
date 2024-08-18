a35cdae624f0cec3c09832b3a767169c1284290d
harastw <94912005+harastw@users.noreply.github.com>
2024-08-18 18:26:32 +0700

    Update UserItem.vue

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { NInput, NSelect, NButton } from 'naive-ui'
-import { ref, defineProps } from 'vue'
+import { NInput, NSelect, NButton, NForm } from 'naive-ui'
+import { ref, defineProps, watch } from 'vue'
 import UserClass from '../UserClass'
 
 const props = defineProps<{
@@ -12,32 +12,54 @@ const options = [
   { label: 'LDAP', value: 'LDAP' },
   { label: 'Локальная', value: 'Локальная' }
 ]
-
 const form = ref(props.user)
+const isError = ref(false)
+const validateInput = () => {
+  isError.value = form.value.password.length < 8
+}
 </script>
 
 <template>
   <li class="user-item">
     <div class="form-group">
-      <n-input placeholder="Tag" maxlength="20" v-model="form.tag" />
+      <n-input
+        placeholder="Tag"
+        maxlength="20"
+        v-model="form.tag"
+        :default-value="props.user.tag"
+      />
     </div>
 
     <div class="form-group">
-      <n-select placeholder="Выберите тип" v-model="form.type" :options="options" />
-    </div>
-
-    <div class="form-group">
-      <n-input placeholder="Login" maxlength="20" v-model="form.login" />
+      <n-select
+        placeholder="Выберите тип"
+        v-model="form.type"
+        :options="options"
+        :default-value="props.user.type"
+      />
     </div>
 
     <div class="form-group">
       <n-input
+        placeholder="Login"
+        maxlength="20"
+        v-model="form.login"
+        :default-value="props.user.login"
+      />
+    </div>
+
+    <n-form :model="form" class="form-group">
+      <n-input
         type="password"
         placeholder="Введите пароль"
         maxlength="20"
-        v-model="form.password"
+        v-model:value="form.password"
+        :default-value="props.user.password"
+        @input="validateInput"
+        :status="isError ? 'error' : 'success'"
       />
-    </div>
+      <span v-if="isError" style="color: red">Пароль должен быть не менее 8 символов</span>
+    </n-form>
 
     <div>
       <n-button type="error" @click="onRemove">Remove</n-button>
