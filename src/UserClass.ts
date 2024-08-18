cf9b9767c5129ca0d4f6884e6692f3cabbd3ad56
harastw <94912005+harastw@users.noreply.github.com>
2024-08-18 21:26:20 +0700

    Update UserClass.ts

--- src/UserClass.ts
+++ src/UserClass.ts
@@ -3,4 +3,10 @@ export default class UserClass {
   type: string = 'LDAP'
   login: string = ''
   password: string = ''
+  constructor(tag: string = '', type: string = '', login: string = '', password: string = '') {
+    this.tag = tag
+    this.type = type
+    this.login = login
+    this.password = password
+  }
 }
